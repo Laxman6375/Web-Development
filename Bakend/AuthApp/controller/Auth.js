b6eedaf52442bfbf4ccc00eb273976038c418da0
Laxman6375 <laxmansuthar365@gmail.com>
2023-10-21 21:52:32 +0530

    implemented authorization and authentication

--- Bakend/AuthApp/controller/Auth.js
+++ Bakend/AuthApp/controller/Auth.js
@@ -1,5 +1,8 @@
 const bcrypt = require("bcrypt");
 const User = require("../model/User");
+const jwt = require("jsonwebtoken");
+const { use, options } = require("../routes/user");
+require("dotenv").config();
 
 //sign route handler
 exports.signup = async (req, res) => {
@@ -42,6 +45,70 @@ exports.signup = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "User can't be reistered, please try again later",
+    });
+  }
+};
+
+//login
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //validation on email and password
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide an email and a password",
+      });
+    }
+
+    //check for registered user
+    let user = await User.findOne({ email });
+    //if not a registered user
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    const payload = {
+      id: user._id,
+      role: user.role,
+      email: user.email,
+    };
+    //verify password and generate a jwt token
+    if (await bcrypt.compare(password, user.password)) {
+      //password matched
+      let token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "2h",
+      });
+
+      user = user.toObject();
+      user.token = token;
+      user.password = undefined;
+
+      const options = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 1000),
+        httpOnly: true,
+      };
+      res.cookie("token", token, options).status(200).json({
+        success: true,
+        token,
+        user,
+        message: "User logged in successfully",
+      });
+    } else {
+      //password do not matched
+      return res.status(401).json({
+        success: false,
+        message: "Invalid Password",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "login failure",
     });
   }
 };
--- Bakend/AuthApp/routes/user.js
+++ Bakend/AuthApp/routes/user.js
@@ -2,8 +2,31 @@ const express = require("express");
 const router = express.Router();
 
 const { login, signup } = require("../controller/Auth");
+const { auth, isStudent, isAdmin } = require("../middlewares/auth");
 
-// router.post("/login", login);
+router.post("/login", login);
 router.post("/signup", signup);
+
+//protected route
+router.get("/student", auth, isStudent, (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to student area",
+  });
+});
+
+router.get("/test", auth, (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to test area",
+  });
+}); 
+
+router.get("/admin", auth, isAdmin, (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to Admin area",
+  });
+});
 
 module.exports = router;
