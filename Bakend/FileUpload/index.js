df2a77d95c9cfcdb0aa99725b5f622327f51d96a
Laxman6375 <laxmansuthar365@gmail.com>
2023-11-19 21:32:41 +0530

    file upload in local

---
+++ Bakend/FileUpload/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+const fileupload = require("express-fileupload");
+app.use(fileupload());
+
+//db se connection
+const db = require("./config/database");
+db.connect();
+
+//cloud se connect kar rahe h
+const cloudinary = require("./config/cloudinary");
+cloudinary.cloudinaryConnect();
+
+const Upload = require("./routes/FileUpload");
+app.use("/api/v1/upload", Upload);
+
+app.listen(PORT, () => {
+  console.log(`app is running at ${PORT}`);
+});
